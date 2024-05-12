bfe9d0bd504550bbeae627f87d72cd8a76e42635
Nazariy Chetvertukha <nazariy.chetvertukha@gmail.com>
2024-05-12 07:09:32 +0300

    fix build

---
+++ Finder.Data/Migrations/20240512040916_RenameColumn.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Finder.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CanCreateHelpRequest",
+                schema: "dbo",
+                table: "Roles",
+                newName: "CanCreateSearchOperation");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CanCreateSearchOperation",
+                schema: "dbo",
+                table: "Roles",
+                newName: "CanCreateHelpRequest");
+        }
+    }
+}
